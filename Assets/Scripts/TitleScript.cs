5fc6ef86b00cf6d4cdc8fb437b03fb9baaa380b7
KoriganStone <Kstone22@wustl.edu>
2016-11-06 21:18:46 -0800

    Added title and background\!

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleScript : MonoBehaviour {
+
+    public GameObject closedBox;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDown()
+    {
+        gameObject.SetActive(false);
+        closedBox.SetActive(true);
+    }
+
+}
